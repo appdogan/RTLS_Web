05fcef70e5503d1af8a4eabf16a871a5b08e5fee
Onur DOĞAN <dgnnr95@gmail.com>
2018-07-03 13:58:34 +0300

    Update ws.js

--- Server/ws.js
+++ Server/ws.js
@@ -8,7 +8,7 @@ io.sockets.on('connection', function (socket) {
         if (data.length == 4) {
             console.log(data[0] + "," + data[1] + "," + data[2] + "," + data[3]);
             socket.emit('mesajgitti', data[0] + "," + data[1] + "," + data[2] + "," + data[3]);
-        } else if (data.length == 13) {
+        } else if (data.length != 15) {   //changed  by Onur
             console.log(data[1] + "," + data[2] + "," + data[3] + "," + data[4] + "," + data[5] + "," + data[6] + "," + data[7] + "," + data[8] + "," + data[9] + "," + data[10] + "," + data[11]);
             socket.emit('mesajgitti', data[1] + "," + data[2] + "," + data[3] + "," + data[4] + "," + data[5] + "," + data[6] + "," + data[7] + "," + data[8] + "," + data[9] + "," + data[10] + "," + data[11]);
         } else {
